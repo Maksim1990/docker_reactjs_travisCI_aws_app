6859b241d779e7c0d2db7f292d1bd17ac651fa95
Maksim <narushevich.maksim@gmail.com>
2019-02-09 15:47:35 +0300

    Updated index file text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            LEARN DOCKER!!
+            Test AWS ReactJS App!
           </p>
           <a
             className="App-link"
